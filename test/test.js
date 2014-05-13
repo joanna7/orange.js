509e297aefb7898fd1984ddf840b833bccfc8e86
Shuang Wu <wushuang_1227@163.com>
2014-05-13 23:02:08 +0800

    modify tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,16 @@
-orange('ul').addEvent('mouseenter', function() {
-  console.log('in');
-  this.style.backgroundColor = 'red';
-}).addEvent('mouseleave', function() {
-  this.style.backgroundColor = '#fff';
+module('selector tests');
+test('tag selector', function() {
+  equal(document.getElementById('idUl'), orange('ul').el[0]);
+});
+
+test('id selector', function() {
+  equal(document.getElementById('idUl'), orange('#idUl').el[0]);
+});
+
+test('class selector', function() {
+  equal(document.getElementById('idUl'), orange('.classUl').el[0]);
+});
+
+test('chained selector', function() {
+  equal(document.getElementById('idUl'), orange('#container .classUl').el[0]);
 });
